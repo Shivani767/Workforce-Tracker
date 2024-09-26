7619b2544fdb6b998cb0f785a96dae7e163e60ad
Shivani Bhandari <shivanibhandari767@gmail.com>
2024-09-26 23:21:06 +0530

    Update ToDo.tsx

--- src/pages/sections/ToDo.tsx
+++ src/pages/sections/ToDo.tsx
@@ -1,16 +1,25 @@
-const ToDo = () => {
-  // ...
+// import { todoData } from "@/data/index";
+import { Card, CardContent } from "@/components/ui/card";
+import { HiDotsVertical } from "react-icons/hi";
+import { FaPlus } from "react-icons/fa6";
+import { DoingData } from "@/data/index";
+import { useTasks } from "@/context/AppContext";
+import { CiCircleCheck } from "react-icons/ci";
+import { RiContactsLine } from "react-icons/ri";
 
+const ToDo = () => {
+  const { tasks, handleOpenForm, handleEditTask } = useTasks();
+  console.log(tasks, "sandhya");
   return (
     <div className="flex flex-col gap-2  rounded-lg ">
       <div className="flex gap-2 items-center justify-between ">
         <div className="flex gap-2 items-center">
-          <p className="text-blue-600 text-lg ">To Do</p>
-          <p className="text-purple-500">
+          <p className="text-white text-lg ">To Do</p>
+          <p className="text-gray-400">
             {tasks.filter((task) => task.status === "to do").length + 1}
           </p>
         </div>
-        <div className="flex gap-4 text-blue-600 ">
+        <div className="flex gap-4 text-white ">
           <FaPlus onClick={handleOpenForm} />
           <HiDotsVertical />
         </div>
@@ -18,24 +27,24 @@ const ToDo = () => {
       {DoingData.map((item) => (
         <Card
           key={item.id}
-          className="section border-slate-800 rounded-lg p-2 cursor-pointer hover:border-slate-600 transition "
+          className=" section border-zinc-700 rounded-lg p-2 cursor-pointer hover:border-zinc-500 transition "
         >
           <CardContent className="flex items-center p-2 gap-2">
             <p className=" text-lg">{item.icon1}</p>
-            <p className="text-base text-blue-600">{item.text}</p>
+            <p className="text-base text-white">{item.text}</p>
           </CardContent>
           <CardContent className="space-y-2 p-2">
             <div className="flex gap-2">
-              <p className="rounded-full text-black text-xs px-3 py-0.5 items-center justify-center flex bg-pink-500">
+              <p className="rounded-full text-black text-xs px-3 py-0.5 items-center justify-center flex bg-orange-400">
                 Medium
               </p>
-              <p className="rounded-full text-black text-xs px-3 py-0.5 items-center justify-center flex bg-teal-400">
+              <p className="rounded-full text-black text-xs px-3 py-0.5 items-center justify-center flex bg-yellow-400">
                 At risk
               </p>
             </div>
             <div className="flex items-center justify-start gap-4">
-              <p className=" text-2xl text-purple-500">{item.icon2}</p>
-              <p className=" text-purple-500">{item.date}</p>
+              <p className=" text-2xl text-gray-400">{item.icon2}</p>
+              <p className=" text-gray-400">{item.date}</p>
             </div>
           </CardContent>
         </Card>
@@ -45,7 +54,7 @@ const ToDo = () => {
         .map((task, index) => (
           <Card
             key={index}
-            className="section border-slate-800 rounded-lg p-2 cursor-pointer hover:border-slate-600 transition"
+            className="section border-zinc-700 rounded-lg p-2 cursor-pointer hover:border-zinc-500 transition"
             onClick={() => handleEditTask(task.id)}
           >
             <CardContent className="flex items-center p-2 gap-2">
@@ -53,8 +62,8 @@ const ToDo = () => {
               <p className="text-lg">{task.title}</p>
             </CardContent>
             <CardContent className="space-y-2 p-2">
-              <p className="text-base text-blue-600">{task.description}</p>
-              <div className="flex items-center justify-start gap-4 text-purple-500">
+              <p className="text-base text-white">{task.description}</p>
+              <div className="flex items-center justify-start gap-4 text-gray-400">
                 <RiContactsLine size={20} />
                 <p>{task.date}</p>
               </div>
@@ -62,7 +71,7 @@ const ToDo = () => {
           </Card>
         ))}
       <p
-        className="text-center font-semibold text-purple-500 cursor-pointer"
+        className="text-center font-semibold text-gray-400 cursor-pointer"
         onClick={handleOpenForm}
       >
         + Add task
