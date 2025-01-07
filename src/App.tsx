81af3b4286f204a7dd44521c7f22dcb244e4b421
Shivani Bhandari <shivanibhandari767@gmail.com>
2025-01-07 23:53:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/sections/Home";
 import Page from "./pages/Page";
+import Dashboard from "./pages/sections/Dashboard"; // Import the Dashboard component
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Nested routes with Layout */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/page/:id" element={<Page />} />
+          <Route path="/page/dashboard" element={<Dashboard />} /> {/* Add Dashboard route */}
         </Route>
       </Routes>
     </>
